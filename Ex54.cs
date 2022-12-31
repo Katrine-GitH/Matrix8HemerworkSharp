78d124c46fe7e7213192ddd7b7e945bf8db24c78
Katrine-GitH <119107912+Katrine-GitH@users.noreply.github.com>
2022-12-31 19:01:23 +0300

    Add files via upload

---
+++ Ex54.cs
@@ -0,0 +1,84 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки 
+//двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+///summary
+/// Заполнили двухмерный массив рандомными числами от а до б
+///summary
+int[,] NumberRandom(int a, int b)
+{
+    int[,] Array = new int[a, b];
+    Random random = new Random();
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            Array[i, j] = random.Next(10);
+        }
+    }
+    return Array;
+}
+///summary
+///Упорядочивание элементов в строках по убыванию в двухмерном массиве
+///summary
+int[,] DescendiNumber(int[,] Array)
+{
+
+    int col = Array.GetLength(1);
+    int row = Array.GetLength(0);
+
+    //перебор строк
+    for (int i = 0; i < row; i++)
+    {
+        for (int r = 0; r < col - 1; r++)
+        {
+            //перебор столбцов (эле-ментов сторки)
+            for (int j = 0; j < col - 1; j++)
+            {
+                if (Array[i, j] < Array[i, j + 1])
+                {
+                    int number;
+                    number = Array[i, j];
+                    Array[i, j] = Array[i, j + 1];
+                    Array[i, j + 1] = number;
+                }
+            }
+        }
+    }
+    return Array;
+}
+///symmary
+///Печать двухмерного массива
+///summary
+void PrintArray(int[,] Array)
+{
+    int col = Array.GetLength(1);
+    int row = Array.GetLength(0);
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            Console.Write($"{Array[i, j]} ");
+        }
+        Console.WriteLine();
+        //return 0;
+    }
+}
+//NumberRandom(5, 10);
+//Console.WriteLine();
+//Console.WriteLine();
+int[,] A = NumberRandom(5, 10);
+PrintArray(A);
+
+Console.WriteLine();
+
+int[,] D = DescendiNumber(A);
+PrintArray(D);
+// Console.WriteLine(NumberRandom(1,4));
+//void Descendingnumber (char [,] Array= new char [1,5])
---
+++ Ex58.cs
@@ -0,0 +1,84 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+//Метод с расширением стр
+// Метод для расшиерения количества строк матрицы
+int RowsCount(int[,] matrix)
+{
+    return matrix.GetUpperBound(0) + 1;
+}
+// Метод для расшиерения количества столбцов матрицы
+int ColumnsCount(int[,] matrix)
+{
+    return matrix.GetUpperBound(0) + 1;
+}
+
+
+//Метод получения матрица с заданными из консоли паараметрами
+static int[,] GetMatrixFromConsole(string name)
+{
+    Console.WriteLine("Введите количество строк матрицы: ", name);
+    var row = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите количество столбцов матрицы: ", name);
+    var col = int.Parse(Console.ReadLine());
+    var matrix = new int[row, col];
+    Random random = new Random();
+    for (var i = 0; i < row; i++)
+    {
+        for (var j = 0; j < col; j++)
+        {
+            matrix[i, j] = random.Next(10);
+        }
+    }
+    return matrix;
+}
+//Метод печати матрицы
+void PrintMatrix(int[,] matrix)
+{
+    int col = matrix.GetLength(1);
+    int row = matrix.GetLength(0);
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+        //return 0;
+    }
+}
+//
+int[,] MatrixProduct(int[,] matrixA, int[,] matrixB)
+{
+    int col = matrixA.GetLength(1);
+    int row = matrixB.GetLength(0);
+    var matrixC = new int[col, row];
+    for (var i = 0; i < row; i++)
+    {
+        for (var j = 0; j < col; j++)
+        {
+            matrixC[i, j] = 0;
+            for (int k = 0; k < col; k++)
+            {
+                matrixC[i, j] += matrixA[i, k] * matrixB[k, j];
+            }
+        }
+    }
+    return matrixC;
+}
+var A = GetMatrixFromConsole("A");
+var B = GetMatrixFromConsole("B");
+Console.WriteLine("Матрица A:");
+PrintMatrix(A);
+Console.WriteLine("Матрица B:");
+PrintMatrix(B);
+Console.WriteLine();
+var result = MatrixProduct(A, B);
+PrintMatrix(result);
+
+
